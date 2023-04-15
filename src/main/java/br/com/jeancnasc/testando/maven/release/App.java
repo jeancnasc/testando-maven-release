bdc5a87e7ddc97290815fa7aa357edcb7ef35163
Jean Nascimento <jeancnasc@hotmail.com>
2023-04-15 14:08:48 -0300

    versão inicial

---
+++ src/main/java/br/com/jeancnasc/testando/maven/release/App.java
@@ -0,0 +1,16 @@
+package br.com.jeancnasc.testando.maven.release;
+
+public class App
+{
+
+
+    public static void main( String[] args )
+    {
+        System.out.println(new App().mensagem());
+    }
+
+    public String mensagem() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/test/java/br/com/jeancnasc/testando/maven/release/AppTest.java
@@ -0,0 +1,15 @@
+package br.com.jeancnasc.testando.maven.release;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest
+{
+
+
+    @Test
+    public void test(){
+        assertThat(new App().mensagem()).isEqualTo("Hello World!");
+    }
+}
